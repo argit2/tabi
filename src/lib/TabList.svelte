9edd77a226597704ffb97f4eb280d3aac3358ba5
argit2 <38925828+argit2@users.noreply.github.com>
2022-08-15 21:51:45 -0300

    Clicking tab in the list switches to it Tab hover effect and cursor

--- src/lib/TabList.svelte
+++ src/lib/TabList.svelte
@@ -1,16 +1,19 @@
 <script>
 export let tabList;
+function onClick(tabId) {
+    chrome.tabs.update(tabId, {active: true})
+}
 </script>
 
 <div class="flex-col max-w-md">
     <div class="flex flex-row ml-4 mr-4 text-lg">{ tabList.title }</div>
     <div class="border border-black rounded divide-y">
-        {#each tabList.tabs as { title, url, favIconUrl}, i}
-        <div class="flex flex-row ml-4 mr-4">
-            <div class="w-full overflow-hidden">
+        {#each tabList.tabs as { title, url, favIconUrl, id}, i}
+        <div class="flex flex-row ml-4 mr-4 cursor-pointer hover:border-l-slate-500 border-l-2 border-l-transparent" on:click={() => onClick(id)}>
+            <div class="w-full overflow-hidden ml-1">
                 <div class="flex flex-row w-full">
-                    <div class="block w-4 h-4 min-w-4 min-h-4 self-center">
-                        <img class="w-full h-full self-center" src="{favIconUrl}" alt="" title="{title}">
+                    <div class="block w-4 h-4 min-w-max min-h-max self-center">
+                        <img class="w-4 h-4 self-center" src="{favIconUrl}" alt="" title="{title}">
                     </div>
                     <div class="text-base text-truncate whitespace-nowrap w-full" title="{title}">{title}</div>
                 </div>
--- src/lib/TabLists.svelte
+++ src/lib/TabLists.svelte
@@ -3,7 +3,7 @@
     import {tabLists} from '../stores.js';
 </script>
 
-<div class="grid grid-cols-4">
+<div class="grid grid-cols-4 gap-4">
     {#each $tabLists as tabList, i}
     <TabList tabList={tabList} />
     {/each}

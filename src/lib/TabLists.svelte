8dd620bb6f7ad7b1df8398068f7cf7dc7b3795a3
argit2 <38925828+argit2@users.noreply.github.com>
2022-09-12 21:15:29 -0300

    Decreased gap between lists on left panel

--- src/lib/TabLists.svelte
+++ src/lib/TabLists.svelte
@@ -4,7 +4,7 @@
     import {currentTab} from '../stores.js';
 </script>
 
-<div class="grid md:grid-cols-4 md:grid-rows-2 grid-cols-1 grid-rows-8 md:gap-4 h-screen max-h-screen auto-rows-fr">
+<div class="grid md:grid-cols-4 md:grid-rows-2 grid-cols-1 grid-rows-8 gap-0 md:gap-4 h-screen max-h-screen auto-rows-fr">
     {#each [...$tabLists, $importantTabs, $toReadTabs] as tabList, i}
     <TabList tabList={tabList} currentTab={$currentTab}/>
     {/each}
